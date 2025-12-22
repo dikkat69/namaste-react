c8d014e2e7653ca3532f5f7b4b7d10613344c525
dikkat69 <ui23cs69@iiitsurat.ac.in>
2025-12-22 15:46:18 +0530

    Updating the Learning Project

---
+++ dist/React-js.6bd02f5a.js
@@ -0,0 +1,21 @@
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child1"
+    }, [
+        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h2 tag")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h2 tag")
+    ])
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent);
+root.render(parent);
+
+//# sourceMappingURL=React-js.6bd02f5a.js.map
